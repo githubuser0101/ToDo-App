1a312a21851053173ade968bf20a74742e3aeb26
githubuser0101 <nowmailsonuchauhan@gmail.com>
2025-01-14 23:37:06 +0530

    Fixed initial completed status

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -10,7 +10,7 @@ function TodoForm() {
 
         if (!todo) return
 
-        addTodo({todoMsg: todo, completed: true})
+        addTodo({todoMsg: todo, completed: false})
         setTodo("")
     }
 
